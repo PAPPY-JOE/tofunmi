8676c14c23fabeca0f53fbdcd6c5265bc9aa1ed2
PAPPY-JOE <fatoyejoseph@gmail.com>
2022-08-22 19:51:42 +0100

    created form

--- scripts/scroll-reveal-test.js
+++ scripts/scroll-reveal-test.js
@@ -15,3 +15,6 @@ sr.reveal('.next-header p', {reset: false});
 sr.reveal('.left-btn', {distance: '60px', duration: 1200,});
 sr.reveal('.about-item', {distance: '60px', interval: 400,});
 sr.reveal('.next-item-box', {distance: '60px', interval: 300,});
+sr.reveal('.contact .form-input input', {distance: '60px', interval: 300,});
+sr.reveal('.contact .form-input textarea', {distance: '60px', delay: 800,});
+sr.reveal('.contact-left-align h2', {distance: '35px', delay: 200,});
