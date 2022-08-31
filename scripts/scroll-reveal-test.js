9d03c8b7296d1480559947c42585a69ffce506d1
PAPPY-JOE <fatoyejoseph@gmail.com>
2022-08-31 17:52:39 +0100

    updated abouts section majorly, responsiveness upgrade

--- scripts/scroll-reveal-test.js
+++ scripts/scroll-reveal-test.js
@@ -10,6 +10,7 @@ let sr = ScrollReveal({
 
 // Scroll titles
 sr.reveal('.next-header p', {reset: false});
+sr.reveal('.a-section-text-container .point', {distance: '35px', delay: 200, reset: false,});
 
 // Scroll boxes
 sr.reveal('.left-btn', {distance: '60px', duration: 1200,});
@@ -18,6 +19,5 @@ sr.reveal('.next-item-box', {distance: '60px', interval: 300,});
 sr.reveal('.contact .form-input input', {distance: '60px', interval: 300,});
 sr.reveal('.contact .form-input textarea', {distance: '60px', delay: 800,});
 sr.reveal('.contact-left-align h2', {distance: '35px', delay: 200,});
-sr.reveal('.a-section-text-container h2', {distance: '60px', delay: 280,});
+// sr.reveal('.a-section-text-container h2', {distance: '60px', delay: 280,});
 sr.reveal('.a-section-text-container .intro', {distance: '60px', delay: 200,});
-sr.reveal('.a-section-text-container .point', {distance: '35px', delay: 200,});
